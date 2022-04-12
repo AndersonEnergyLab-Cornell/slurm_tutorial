9fd68a7d19747c8388092035bffed2f0477a64e5
Vivienne16 <ml2589@cornell.edu>
2022-04-11 21:37:10 -0400

    fixed python_slurm for hopper

--- python_slurm.sh
+++ python_slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="pythontest"
-#SBATCH --output="pythontest.out"
+#SBATCH --output=pythontest.%A_%a.out
+#SBATCH --arra=1-3
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --exclusive
@@ -8,4 +9,5 @@
 #SBATCH -t 1:00:00
 
 cd $SLURM_SUBMIT_DIR
+module load gnu9/9.3.0
 python3 helloworld.py
